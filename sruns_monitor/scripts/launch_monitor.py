24e76ce0b1cf17c23ea838e48fda7d916cfc5bca
Watson <s0302521@lap13998m.enterprise.stanfordmed.org>
2019-05-30 17:41:50 -0700

    Updated test

--- sruns_monitor/scripts/launch_monitor.py
+++ sruns_monitor/scripts/launch_monitor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 
 from sruns_monitor.monitor import Monitor
--- sruns_monitor/tests/test_utils.py
+++ sruns_monitor/tests/test_utils.py
@@ -14,6 +14,7 @@ Tests functions in the ``sruns_monitor.utils`` module.
 import hashlib
 import json
 import os
+import tarfile
 import unittest
 
 from sruns_monitor.tests import WATCH_DIR, TMP_DIR
@@ -27,25 +28,23 @@ class TestUtils(unittest.TestCase):
 
     def setUp(self):
         self.test_rundir = os.path.join(WATCH_DIR, "CompletedRun1")
-        self.test_tarfile_rundir = os.path.join(WATCH_DIR, "test_run_dir.tar.gz")
-
-    def get_md5sum(self, infile):
-        m = hashlib.md5()
-        fh = open(infile, 'rb')
-        m.update(fh.read())
-        fh.close()
-        return m.hexdigest()
 
     def test_tar(self):
         """
-        Tests the function `utils.tar()` for success. Calls the function to tar the test directory
-        at `self.test_rundir` and write the tarball to TMP_DIR. Then checks whether the md5sum of 
-        that output tarball is equal to the md5sum of a manually created tarball within WATCH_DIR.
+        Tests that `utils.tar()` doesn't miss any files in the tarball. Tars the run directory at
+        `self.test_rundir` and compares the list of files in the tarball with that which we expect
+        to find. Removes the tarball it creates before exiting. 
         """
-        output_file = os.path.join(TMP_DIR, self.test_rundir + ".tar.gz")
+        output_file = os.path.join(TMP_DIR, os.path.basename(self.test_rundir + ".tar.gz"))
         utils.tar(input_dir=self.test_rundir, tarball_name=output_file)
-        md5_output_file = self.get_md5sum(output_file)
-        self.assertEqual(md5_output_file, self.get_md5sum(os.path.join(WATCH_DIR, "CompletedRun1.tar.gz")))
+        t = tarfile.open(output_file)
+        file_list = t.getnames()
+        expected_file_list = [
+            "CompletedRun1",
+            "CompletedRun1/CopyComplete.txt"
+        ]
+        os.remove(output_file)
+        self.assertEqual(file_list, expected_file_list)
 
 
 
